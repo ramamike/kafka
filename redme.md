88f6472f80cee0ab003c5a37641c8e2568ad2e69
ramamike <7629756@gmail.com>
2022-09-21 23:48:20 +0300

    init: start javaproject

---
+++ redme.md
@@ -0,0 +1,4 @@
+1. sudo systemctl start zookeeper.service
+2. sudo systemctl start kafka.service
+3. sudo systemctl status kafka zookeeper.service
+4. 
---
+++ src/main/java/org/learning/kafka/Main.java
@@ -0,0 +1,9 @@
+package org.learning.kafka;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+
+    }
+}
